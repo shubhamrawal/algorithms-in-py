de0027e076ac9941e7690f9b38dd5d4fcf87be71
Shubham Rawal <srawal@student.unimelb.edu.au>
2020-03-05 15:28:28 +1100

    removed semicolons

--- algorithms/ds/linkedlist.py
+++ algorithms/ds/linkedlist.py
@@ -1,133 +1,133 @@
 class Node:
   def __init__(self, val: int):
-    self.val = val;
-    self.next = None;
+    self.val = val
+    self.next = None
 
 class LinkedList:
   def __init__(self, head: Node):
-    self.head = head;
-    self.end = head;
-    self.size = 0 if not head else 1;
+    self.head = head
+    self.end = head
+    self.size = 0 if not head else 1
 
   def __str__(self):
-    repr = '';
-    currentNode = self.head;
+    repr = ''
+    currentNode = self.head
     if currentNode:
-      repr += str(currentNode.val);
-      currentNode = currentNode.next;
+      repr += str(currentNode.val)
+      currentNode = currentNode.next
     else:
-      return repr;
+      return repr
     
     while(currentNode):
-      repr += '->' + str(currentNode.val);
-      currentNode = currentNode.next;
+      repr += '->' + str(currentNode.val)
+      currentNode = currentNode.next
 
-    return repr;
+    return repr
 
   def append(self, val: int) -> Node:
     if val:
-      node = Node(val);
-      self.end.next = node;
-      self.end = node;
-      self.size += 1;
-      return node;
+      node = Node(val)
+      self.end.next = node
+      self.end = node
+      self.size += 1
+      return node
 
-    return None;
+    return None
 
   def pop(self, index = None) -> Node:
     if index is None:
-      index = self.size - 1;
+      index = self.size - 1
     if index < self.size:
       if index == 0:
-        oldHead = self.head;
-        self.head = self.head.next;
-        oldHead.next = None;
-        return oldHead;
+        oldHead = self.head
+        self.head = self.head.next
+        oldHead.next = None
+        return oldHead
 
-      currentNode = self.head;
-      prevNode = None;
-      count = 1;
+      currentNode = self.head
+      prevNode = None
+      count = 1
       while(count <= index):
-        prevNode = currentNode;
-        currentNode = currentNode.next;
-        count += 1;
+        prevNode = currentNode
+        currentNode = currentNode.next
+        count += 1
 
-      prevNode.next = currentNode.next;
-      currentNode.next = None;
+      prevNode.next = currentNode.next
+      currentNode.next = None
       if not prevNode.next:
-        self.end = prevNode;
+        self.end = prevNode
 
-      return currentNode;
+      return currentNode
 
   def remove(self, val: int) -> Node:
-    currentNode = self.head;
-    index = 0;
+    currentNode = self.head
+    index = 0
     while(currentNode):
       if currentNode.val == val:
-        return self.pop(index);
+        return self.pop(index)
       
-      currentNode = currentNode.next;
-      index += 1;
+      currentNode = currentNode.next
+      index += 1
 
-    return None;
+    return None
 
   def prepend(self, val: int) -> Node:
     if val:
-      node = Node(val);
-      node.next = self.head;
-      self.head = node;
-      self.size += 1;
-      return node;
+      node = Node(val)
+      node.next = self.head
+      self.head = node
+      self.size += 1
+      return node
 
-    return None;
+    return None
 
   def insert(self, index: int, val: int) -> Node:
     if val and index < self.size:
       if index == 0:
-        return self.prepend(val);
+        return self.prepend(val)
       
-      node = Node(val);
-      currentNode = self.head;
-      count = 1;
+      node = Node(val)
+      currentNode = self.head
+      count = 1
       while(count < index):
-        currentNode = currentNode.next;
-        count += 1;
+        currentNode = currentNode.next
+        count += 1
 
-      node.next = currentNode.next;
-      currentNode.next = node;
-      self.size += 1;
-      return node;
+      node.next = currentNode.next
+      currentNode.next = node
+      self.size += 1
+      return node
 
-    return None;
+    return None
 
   def search(self, val: int) -> Node:
-    currentNode = self.head;
+    currentNode = self.head
     while(currentNode):
       if currentNode.val == val:
-        return currentNode;
-      currentNode = currentNode.next;
+        return currentNode
+      currentNode = currentNode.next
     
-    return None;
+    return None
 
 if __name__ == '__main__':
-  head = Node(5);
-  linked = LinkedList(head);
-  print(linked);
-  linked.append(7);
-  print(linked);
-  linked.prepend(1);
-  print(linked);
-  linked.insert(1, 3);
-  print(linked);
+  head = Node(5)
+  linked = LinkedList(head)
+  print(linked)
+  linked.append(7)
+  print(linked)
+  linked.prepend(1)
+  print(linked)
+  linked.insert(1, 3)
+  print(linked)
 
-  val = 5;
-  node = linked.search(val);
+  val = 5
+  node = linked.search(val)
   if node and node.next:
-    print(f'The value of the node after {str(node.val)} is: {str(node.next.val)}');
+    print(f'The value of the node after {str(node.val)} is: {str(node.next.val)}')
 
-  linked.pop();
-  print(linked);
-  linked.append(7);
-  print(linked);
-  linked.remove(5);
-  print(linked);
+  linked.pop()
+  print(linked)
+  linked.append(7)
+  print(linked)
+  linked.remove(5)
+  print(linked)
