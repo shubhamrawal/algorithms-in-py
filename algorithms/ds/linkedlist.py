81177cfdfca5c4a625c28ceea4de2079ab6d8e32
Shubham Rawal <srawal@student.unimelb.edu.au>
2020-03-05 16:39:46 +1100

    added tests for linked lists

--- algorithms/ds/linkedlist.py
+++ algorithms/ds/linkedlist.py
@@ -26,48 +26,17 @@ class LinkedList:
 
   def append(self, val: int) -> Node:
     if val:
+      if not self.head:
+        head = Node(val)
+        self.head = head
+        self.end = head
+        return head
+      
       node = Node(val)
       self.end.next = node
       self.end = node
       self.size += 1
       return node
-
-    return None
-
-  def pop(self, index = None) -> Node:
-    if index is None:
-      index = self.size - 1
-    if index < self.size:
-      if index == 0:
-        oldHead = self.head
-        self.head = self.head.next
-        oldHead.next = None
-        return oldHead
-
-      currentNode = self.head
-      prevNode = None
-      count = 1
-      while(count <= index):
-        prevNode = currentNode
-        currentNode = currentNode.next
-        count += 1
-
-      prevNode.next = currentNode.next
-      currentNode.next = None
-      if not prevNode.next:
-        self.end = prevNode
-
-      return currentNode
-
-  def remove(self, val: int) -> Node:
-    currentNode = self.head
-    index = 0
-    while(currentNode):
-      if currentNode.val == val:
-        return self.pop(index)
-      
-      currentNode = currentNode.next
-      index += 1
 
     return None
 
@@ -82,7 +51,7 @@ class LinkedList:
     return None
 
   def insert(self, index: int, val: int) -> Node:
-    if val and index < self.size:
+    if val and (not self.head or 0 <= index <= self.size):
       if index == 0:
         return self.prepend(val)
       
@@ -100,6 +69,47 @@ class LinkedList:
 
     return None
 
+  def pop(self, index = None) -> Node:
+    if index is None:
+      index = self.size - 1
+    if 0 <= index < self.size:
+      if index == 0:
+        oldHead = self.head
+        self.head = self.head.next
+        oldHead.next = None
+        self.size -= 1
+        return oldHead
+
+      currentNode = self.head
+      prevNode = None
+      count = 1
+      while(count <= index):
+        prevNode = currentNode
+        currentNode = currentNode.next
+        count += 1
+
+      prevNode.next = currentNode.next
+      currentNode.next = None
+      if not prevNode.next:
+        self.end = prevNode
+
+      self.size -= 1
+      return currentNode
+
+    return None
+
+  def remove(self, val: int) -> Node:
+    currentNode = self.head
+    index = 0
+    while(currentNode):
+      if currentNode.val == val:
+        return self.pop(index)
+      
+      currentNode = currentNode.next
+      index += 1
+
+    return None
+
   def search(self, val: int) -> Node:
     currentNode = self.head
     while(currentNode):
@@ -112,22 +122,6 @@ class LinkedList:
 if __name__ == '__main__':
   head = Node(5)
   linked = LinkedList(head)
-  print(linked)
-  linked.append(7)
-  print(linked)
-  linked.prepend(1)
-  print(linked)
-  linked.insert(1, 3)
-  print(linked)
-
-  val = 5
-  node = linked.search(val)
-  if node and node.next:
-    print(f'The value of the node after {str(node.val)} is: {str(node.next.val)}')
-
-  linked.pop()
-  print(linked)
-  linked.append(7)
-  print(linked)
-  linked.remove(5)
+  linked.append(6)
+  linked.insert(2, 7)
   print(linked)
